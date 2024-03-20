bcd1725bfbaca3fd12263a7529badea488d72462
410manan <patelmanan4102002@gmail.com>
2024-03-20 11:05:53 +0530

    third commit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,28 +7,28 @@ function Footer() {
             <div className="container">
                 <div className="row">
                     <div className="col-md-6 col-lg-3 col-sm-6">
-                        <h6 style={{ color: 'white' }}>Useful Links</h6>
+                        <h4>Useful Links</h4>
                         <ul>
-                            <li><a href="#">Home</a></li>
-                            <li><a href="#">Register Now</a></li>
-                            <li><a href="#">Products</a></li>
-                            <li><a href="#">New Arrivals</a></li>
-                            <li><a href="#">Clearance Sale</a></li>
-                            <li><a href="#">Cigar Tax Calculator</a></li>
+                            <a href="#">Home</a><br/>
+                            <a href="#">Register Now</a><br />
+                            <a href="#">Products</a><br />
+                            <a href="#">New Arrivals</a><br />
+                            <a href="#">Clearance Sale</a><br />
+                            <a href="#">Cigar Tax Calculator</a><br/>
                         </ul>
                     </div>
                     <div className="col-md-6 col-lg-3 col-sm-6">
-                        <h6 style={{ color: 'white' }}>About</h6>
+                        <h4>About</h4>
                         <ul>
-                            <li><a href="#">Orders</a></li>
-                            <li><a href="#">Contact Us</a></li>
-                            <li><a href="#">Return Policy</a></li>
-                            <li><a href="#">Privacy Policy</a></li>
-                            <li><a href="#">Stores</a></li>
+                            <a href="#">Orders</a><br/>
+                            <a href="#">Contact Us</a><br/>
+                            <a href="#">Return Policy</a><br/>
+                            <a href="#">Privacy Policy</a><br/>
+                            <a href="#">Stores</a><br/>
                         </ul>
                     </div>
                     <div className="col-md-6 col-lg-3 col-sm-6">
-                        <h6 style={{ color: 'white' }}>Working hours</h6>
+                        <h4>Working hours</h4>
                         <ul>
                             <li>Monday - Friday: <br />
                                 09:00 AM - 07:00 PM<br />
@@ -43,19 +43,13 @@ function Footer() {
                         </ul>
                     </div>
                     <div className="col-md-6 col-lg-3 col-sm-6">
-                        <h6 style={{ color: 'white' }}>Contact Us</h6>
+                        <h4>Contact Us</h4>
                         <ul>
-                            <li className="telNo"><a href="#">(+1) 516 513 1522</a>
-                                <a href="#">(+1) 516 492 4224</a></li>
-                            <li>
+                            <a href="#">(+1) 516 513 1522</a><br/>
+                                <a href="#">(+1) 516 492 4224</a><br/>
                                 libertysmoke@support.com<br />
-                            </li>
-                            <li>
                                 188 Quality Plaza, Hicksville, NY ,11801<br />
-                            </li>
-                            <li>
                                 102 West 29th St, NY, 10001<br />
-                            </li>
                         </ul>
                         <div className="social-media">
                             <a href="#" target="_blank">
@@ -70,10 +64,10 @@ function Footer() {
                         </div>
                     </div>
                 </div>
+                </div>
                 <div className="copyRights">
                     © Liberty Smoke Distributors - All Rights Reserved
                 </div>
-            </div>
         </footer>
     );
 }
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,63 +1,102 @@
-import React from 'react';
-import './Navbar.css'; // Add your CSS styles for styling the navbar
-import NewIcon from './NewIcon' 
-
-function Navbar() {
+import React, { useState } from 'react';
+import NewIcon from './NewIcon'; // Assuming NewIcon component for the icon
+const DropdownContent = () => {
   return (
-    <div className="navbar-full">
-    <nav className="navbar">
-      <div className="navbar-container">
-
-        <ul className="nav-menu">
-          <li className="nav-item">
-            <a href="#" className="nav-links">
-              <NewIcon />
-              New In Stock
-            </a>
+    <div className="dropdown-content d-flex">
+      <div className="dropdown-column">
+        <ul>
+          <li>
+            <a href="#">Item 1</a>
           </li>
-          <li className="nav-item">
-            <a href="#" className="nav-links">
-              Disposables
-            </a>
+          <li>
+            <a href="#">Item 2</a>
           </li>
-          <li className="nav-item">
-            <a href="#" className="nav-links">
-              Devices
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="#" className="nav-links">
-              E-Liquids
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="#" className="nav-links">
-              Pod Systems & Glass
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="#" className="nav-links">
-              Clearance Disposables
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="#" className="nav-links">
-              Brands
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="#" className="nav-links">
-              Promos
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="#" className="nav-links">
-              My Cart (0)
-            </a>
+          <li>
+            <a href="#">Item 3</a>
           </li>
         </ul>
       </div>
-    </nav>
+      <div className="dropdown-column">
+        <ul>
+          <li>
+            <a href="#">Item 4</a>
+          </li>
+          <li>
+            <a href="#">Item 5</a>
+          </li>
+          <li>
+            <a href="#">Item 6</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+function Navbar() {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  return (
+    <div className="navbar-full">
+      <nav className="navbar">
+        <div className="navbar-container">
+
+          <ul className="nav-menu">
+            <li className="nav-item">
+              <a
+                href="#"
+                className="nav-links"
+                onMouseEnter={() => setIsDropdownOpen(true)}
+                onMouseLeave={() => setIsDropdownOpen(false)}
+              >
+                <NewIcon />
+                New In Stock
+                {isDropdownOpen && <DropdownContent />}
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#" className="nav-links">
+                Disposables
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#" className="nav-links">
+                Devices
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#" className="nav-links">
+                E-Liquids
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#" className="nav-links">
+                Pod Systems & Glass
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#" className="nav-links">
+                Clearance Disposables
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#" className="nav-links">
+                Brands
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#" className="nav-links">
+                Promos
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#" className="nav-links">
+                My Cart (0)
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
   );
 }
